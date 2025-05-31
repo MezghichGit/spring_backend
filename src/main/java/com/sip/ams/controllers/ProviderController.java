2295f9d589397bb4c8223b664b983ad7f213ff69
Mohamed Amine MEZGHICH <git@mezghich.com>
2025-05-31 15:06:48 +0100

    delete provider

--- src/main/java/com/sip/ams/controllers/ProviderController.java
+++ src/main/java/com/sip/ams/controllers/ProviderController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -58,6 +59,27 @@ public class ProviderController {
 		}
 		else
 			return "Problem occured when deleting";
+		
+	}
+	
+	@PutMapping("/update/{id}")
+	public Provider updateProvider(@PathVariable("id")  int id, @RequestBody Provider p) {
+		int index=0;
+		
+		for(int i=0;i<providers.size();i++)
+		{
+			if(providers.get(i).getId() == id) {
+				index = i;
+				break;
+			}
+		}
+		
+		Provider temp = new Provider(id,p.getNom(),p.getEmail());
+		
+		providers.set(index, temp);
+		
+		return temp;
+		
 		
 	}
 }
